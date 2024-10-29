faa7ae4b7542edd059a36ad1e876b7049afd32f9
Deepanshu Gupta <dipanshugupta921@gmail.com>
2024-10-29 11:24:28 +0530

    improved ui

--- src/App.jsx
+++ src/App.jsx
@@ -35,12 +35,12 @@ function App() {
         </div>
 
         {/* Content (upper layer) */}
-        <div className="bg-gray-400  bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10   h-[500px] md:h-[640px] w-[320px] rounded-2xl md:w-[550px] mx-auto my-4 border border-purpleColor flex flex-col items-center gap-4">
+        <div className="bg-gray-400 shadow-2xl bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10   h-[500px] md:h-[640px] w-[320px] rounded-2xl md:w-[550px] mx-auto my-4 border border-purpleColor flex flex-col items-center gap-4">
           <h1 className=" text-primaryColor md:text-3xl text-xl mt-8">
             BIHAR CURRENT AFFAIRS
           </h1>
           <h2 className="text-[16px] md:text-[24px] leading-7 md:font-[400] text-wrap">
-            One-Stop Platform for Bihar&apos;s Educational Needs! 📚
+            Your One-Stop Platform for BPSC Aspirants! 📚
           </h2>
 
           <div className="h-[45px]">
